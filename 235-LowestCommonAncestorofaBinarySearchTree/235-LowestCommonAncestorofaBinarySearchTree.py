02f5c4db0ce5d014d4d02a8498e1fd05c0fc623a
RahulV2478 <117884531+RahulV2478@users.noreply.github.com>
2026-05-27 00:22:43 -0500

    File updated at 5/27/2026, 1:22:42 AM

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/27/2026, 1:19:25 AM
+# Last updated: 5/27/2026, 1:22:42 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, x):
@@ -9,22 +9,14 @@
 8class Solution:
 9    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 10        
-11        
-12        
-13        def search(root, p, q):
-14            if root is None:
-15                return None
-16            
-17            if root.val <= p.val and root.val >= q.val:
-18                return root
-19            
-20            elif root.val <= q.val and root.val >= p.val:
-21                return root
-22            
-23            
-24            left = search(root.left, p, q)
-25            right = search(root.right, p, q)
-26            return left or right
-27        
-28        return search(root, p, q)
-29
+11        while root:
+12            if root.val <= p.val and root.val >= q.val:
+13                return root
+14            if root.val <= q.val and root.val >= p.val:
+15                return root
+16            if root.val < q.val and root.val < p.val:
+17                root = root.right
+18            if root.val > q.val and root.val > p.val:
+19                root = root.left
+20
+21
